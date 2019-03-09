12af85b4032e557cc0a506fd126fb0a64e5cda47
fmc <franklin@peeriq.com>
2019-03-09 12:52:24 -0500

    comment

--- Reverse.java
+++ Reverse.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Reverse {
 
+    // Reverse a given array in place
     public static void main(String []args) {
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         int i = 0;
