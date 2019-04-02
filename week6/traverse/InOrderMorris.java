2bcb9713d164e59680e45be905d09cb03fa3989b
fmc <franklin@peeriq.com>
2019-04-01 23:07:34 -0400

    [chore] get Morris traversal working

--- week6/traverse/InOrderMorris.java
+++ week6/traverse/InOrderMorris.java
@@ -1,5 +1,7 @@
 // Similar to in order thread, but does not use boolean isThread indicator
 
+import java.util.Stack;
+
 class Node {
   int data;
   Node left;
@@ -20,86 +22,64 @@ class BTree {
   }
 }
 
-public class Main {
+public class InOrderMorris {
 
   // All leaves (except the last right-most leaf)
   // must have a portal
   private static BTree makeTree() { 
     BTree t = new BTree();
-    t.root = new Node(6);
-    // Tier 2
-    t.root.left = new Node(3);
-    t.root.right = new Node(8);
-
-    // Tier 3
-    Node n3 = t.root.left;
-    Node n8 = t.root.right;
-    n3.left = new Node(1);
-    n3.left.right = n3; // portal
-    n3.right = new Node(5);
-    n3.right.right = t.root; // portal
-    n8.left = new Node(7);
-    n8.left.right = n8; // portal
-    n8.right = new Node(11);
-
-    // Tier 4
-    Node n11 = n8.right;
-    n11.left = new Node(9);
-    n11.left.right = n11; // portal
-    n11.right = new Node(13);
+    t.root = new Node(10);
+    t.root.left = new Node(5);
+    t.root.left.left = new Node(2);
+    t.root.left.right = new Node(7);
+    t.root.right = new Node(15);
+    t.root.right.right = new Node(30);
     return t;
   }
 
-  // return the left most element of a tree,
-  // the left most element of a single node is the node itself
-  private static Node leftMost(Node n) {
-    Node c = n;
-    if (n == null) {
+  // find the predecessor, the right most element of the
+  // left subtree
+  private static Node predecessor(Node n) {
+    if (n.left == null) {
       return null;
     }
-    while (c.left != null) {
-      c = c.left; 
+
+    Node c = null;
+    Stack<Node> s = new Stack<Node>();
+    if (n.left != null) {
+      c = n.left;
     }
-    return c;
-  }
-  
-  
-  // Check if a node n is the in order successor of a given
-  // root node
-  private static Node inOrderSuccessor(Node r, Node n) { 
-    Node s = null;
-    if (n.right != null) {
-        s = leftMost(n);
-    } else {
-        while (r != null) {
-            if (r.data > n.data) {
-                // traverse right
-                r = r.right;
-            } else if (r.data < n.data) {
-                // traverse left
-                s = r;
-                r = r.left;
-            } else {
-                break;
-            }
-        }
+
+    while (c != null) {
+      s.push(c);
+      if (c.right == null) {
+        break;
+      }
+      c = c.right;
     }
-    return s;
+    return s.pop();
   }
   
   // Pass in the root node of the binary tree
-  private static void inOrder(Node root) {
-    if (root == null) { 
-      return;
-    }
-    Node c = leftMost(root);
-    while(c != null) {
-      System.out.println(c.data);
-      if (c.right == inOrderSuccessor(root, c)) {
-        // In the case of a "portal"
+  private static void morrisTraversal(Node r) {
+    Node c = r;
+    while (c != null) {
+      if (c.left == null) {
+        System.out.println(c.data);
         c = c.right;
-      } else { 
-        c = leftMost(c.right);
+        if (c != null) {
+          c.left = null;
+        }
+      } else {
+        Node p = predecessor(c);
+        if (p.right == null) {
+          p.right = c;
+          c = c.left;
+        } else {
+          p.right = null;
+          System.out.println(c.data);
+          c = c.right;
+        }
       }
     }
   }
@@ -107,10 +87,9 @@ public class Main {
   public static void main(String[] args) {
     BTree t = makeTree();
 
-    // test leftMost
-    assert(leftMost(t.root).data == 1);
-    assert(leftMost(t.root.right).data == 7);
+    assert(predecessor(t.root).data == 7); 
+    assert(predecessor(t.root.left).data == 2);
 
-    inOrder(t.root);
+    morrisTraversal(t.root);
   }
 }
