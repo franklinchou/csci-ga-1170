a273289088843f5aa4c1f1a02e8851f097df1bc0
fmc <franklin.chou@yahoo.com>
2019-05-20 00:14:20 -0400

    [chore] simplify code

--- final/IntegerSquareRootLinear.java
+++ final/IntegerSquareRootLinear.java
@@ -3,20 +3,13 @@ public class IntegerSquareRootLinear {
     public static int solve(int n) {
         int i = 1;
         assert(n > 1);
-        while (i < n) {
-            int sq = i * i;
-            if (sq == n) {
-                return i;
-            }
-            if (sq > n) {
-                return i - 1;
-            }
+        while (i * i <= n) {
             i += 1;
         }
-        return i - 1;
+       return i - 1;
     }
     
     public static void main(String[] args) {
-        System.out.println(solve(26));
+        System.out.println(solve(89));
     }
 }
