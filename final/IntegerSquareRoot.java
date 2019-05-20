ba909e35c2bae76e7c12ad5f1c4ac3606650e768
fmc <franklin.chou@yahoo.com>
2019-05-20 08:23:26 -0400

    [chore] Provide loop invariant

--- final/IntegerSquareRoot.java
+++ final/IntegerSquareRoot.java
@@ -18,15 +18,38 @@ public class IntegerSquareRoot {
             // System.out.println("m = " + m);
             int sq = m * m;
             
+            // MAINTENANCE =
+            // If m * m == n, => m = sqrt(n) and n is a perfect
+            // square; lo, hi and m are unchanged and the loop
+            // invariant is unchanged.
             if (sq == n) {
                 System.out.println(m);
                 break;
+            // MAINTENANCE =
+            // If m * m < n => m < sqrt(n); the estimate is LESS
+            // THAN the correct answer, adjust the value of lo s.t.
+            // m + 1 < sqrt(n) <= hi
             } else if (sq < n) {
                 lo = m + 1;
+            // MAINTENANCE =
+            // If m * m > n => m > sqrt(n); the estimate is GREATER
+            // THAN the correct answer, adjust the value of hi s.t.
+            // lo - 1 < sqrt(n) <= m
+            // This satisfies the entry condition, m > sqrt(n).
             } else if (sq > n) {
                 hi = m;
             }
         }
+        // TERMINATION =
+        // At termination lo >= hi due to the loop entry condition.
+        // Combined with the loop invariant, we have:
+        // lo - 1 < sqrt(n) <= hi <= lo
+        // lo - 1 < sqrt(n) <= lo
+        // lo = Ceil(sqrt(n))
+        // n is not a perfect square, otherwise the program
+        // would have terminated in the first `if` condition.
+        // Therefore, sqrt(n) is not an integer and
+        // lo - 1 = Floor(sqrt(n)).
         System.out.println(lo - 1);
     }
     
